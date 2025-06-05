d379b7a3279c7bdb2ed25bc225d0d408ecedaba1
hyamy2025 <smokeless2013@gmail.com>
2025-06-05 10:20:02 +0100

    maj4

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,10 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#1d4ed8',  // bleu vif
-        secondary: '#10b981', // vert doux
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
